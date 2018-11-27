3a702cb1cc432627da9ddd5a0ea9a04157cc573e
yoshi <32868028+yoshi1125hisa@users.noreply.github.com>
2018-11-27 09:48:45 +0900

    [Update] First commit .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { NextPageModule } from '../pages/next/next.module';
 
 @NgModule({
   declarations: [
@@ -14,6 +15,7 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
+    NextPageModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
